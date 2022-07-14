27d5d8f83e64675d0dfd0bb963da653c6a12ffc2
Jose Ortega <link.josef@gmail.com>
2022-07-13 21:18:39 -0500

    - Added more dependencies - Added hilt for dependency injection

---
+++ app/src/main/java/com/android/linkjf/zemoparser/ZemoParserApp.kt
@@ -0,0 +1,7 @@
+package com.android.linkjf.zemoparser
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ZemoParserApp : Application()
--- app/src/main/java/com/android/linkjf/zemoparser/presentation/HomeActivity.kt
+++ app/src/main/java/com/android/linkjf/zemoparser/presentation/HomeActivity.kt
@@ -3,7 +3,9 @@ package com.android.linkjf.zemoparser.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.android.linkjf.zemoparser.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
